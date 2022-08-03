030a0f6de4ef64fd8cec1c2692898539686c0665
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-08-03 19:12:53 -0300

    still in landing page developing for sections

--- app/javascript/packs/components/form_navbar.js
+++ app/javascript/packs/components/form_navbar.js
@@ -1,4 +1,4 @@
-function hideLinks() {
+function hideLinksShowForm() {
 
   const navLinks = document.getElementById('navbar-links');
   const closeBTN = document.getElementById('close-btn');
@@ -11,35 +11,8 @@ function hideLinks() {
   form.classList.toggle('d-none');
 }
 
-// function hideForm() {
-
-//   const navLinks = document.getElementById('navbar-links');
-//   const closeBTN = document.getElementById('close-btn');
-//   const searchBTN = document.getElementById('search-btn');
-//   const form = document.getElementById('navbar-form');
-
-//   let width = parseInt(form.style.width);
-//   width -= 10; // reduce width 10 pixels at a time
-
-//   if (width < 0) width = 0;
-
-//   form.style.width = width + 'px';
-
-//   if (width > 0) {
-//       // keep doing this until width is zero:
-//       setTimeout(collapseForm,100);
-//   }
-//   navLinks.classList.toggle('d-none');
-//   closeBTN.classList.toggle('d-none');
-//   searchBTN.classList.toggle('d-none');
-//   form.classList.toggle('d-none');
-// }
-
-
 const searchBTN = document.getElementById('search-btn');
-
-if (searchBTN) searchBTN.addEventListener('click', hideLinks);
+if (searchBTN) searchBTN.addEventListener('click', hideLinksShowForm);
 
 const closeBTN = document.getElementById('close-btn');
-
-if (closeBTN) closeBTN.addEventListener('click', hideLinks);
+if (closeBTN) closeBTN.addEventListener('click', hideLinksShowForm);
