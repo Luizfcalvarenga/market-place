a57d85c51a01fd63b6e1a63a7791beae44fa1e14
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-08-02 22:17:08 -0300

    navbar search bar

--- app/javascript/packs/components/form_navbar.js
+++ app/javascript/packs/components/form_navbar.js
@@ -2,14 +2,44 @@
 function toggleFormAndLinks() {
 
   const form = document.getElementById('navbar-form');
-  const navLinks = document.getElementById('navbar-links');
-  const closeBTN = document.getElementById('close-btn');
-  const searchBTN = document.getElementById('search-btn');
-  closeBTN.style.display = 'visible';
-  form.style.display = 'visible';
-  navLinks.style.display = 'none';
-  searchBTN.style.display = 'none';
+  form.classList.toggle('d-none')
+  const searchIcon = document.getElementById('search-icon');
+  searchForm.classList.toggle('d-none')
+  const closeIcon = document.getElementById('close-icon');
+  closeIcon.classList.toggle('d-none')
+
 }
+
+
+// function toggleLinks() {
+
+//   const form = document.getElementById('navbar-form');
+//   if (form.style.display === "none") {
+//     form.style.display = "block";
+//   } else {
+//     form.style.display = "none";
+//   }
+//   const navLinks = document.getElementById('navbar-links');
+//   if (navLinks.style.display === "none") {
+//     navLinks.style.display = "block";
+//   } else {
+//     navLinks.style.display = "none";
+//   }
+//   const closeBTN = document.getElementById('close-btn');
+//   if (closeBTN.style.display === "none") {
+//     closeBTN.style.display = "block";
+//   } else {
+//     closeBTN.style.display = "none";
+//   }
+//   const searchBTN = document.getElementById('search-btn');
+//   if (searchBTN.style.display === "none") {
+//     searchBTN.style.display = "block";
+//   } else {
+//     searchBTN.style.display = "none";
+//   }
+// }
+
+
 
 
 
