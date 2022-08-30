25b8603f682e9ddd16bfcbb34afdb6ef710bb6b4
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-08-30 13:52:29 -0300

    card in bikes index in progress

--- app/javascript/react_pages/Bikes.jsx
+++ app/javascript/react_pages/Bikes.jsx
@@ -56,15 +56,36 @@ export function Bikes(props) {
         )}
       </div>
 
-      <div>
+      <div className="row row-cols-1 mt-5">
         {bikes.map((bike) => {
           return (
-            <div className="cards-bikes">
-
-
-              <p>
-                #{bike.id} - {bike.category} - Age: {bike.age} - Size: {bike.size}
-              </p>
+            <div className="col-12 col-md-3">
+              <div className="cards-bikes">
+              <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="bike-card.png" class="d-block w-100 img-card-index" alt="" />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="bike-card.png" class="d-block w-100 img-card-index" alt="" />
+                  </div>
+                  <div class="carousel-item">
+                    <img src="bike-card.png" class="d-block w-100 img-card-index" alt="" />
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+                <p>
+                  #{bike.id} - {bike.category} - Age: {bike.age} - Size: {bike.size}
+                </p>
+              </div>
             </div>
           );
         })}
