ef070f2ecd57c8746767c98acf17cd8a80773fe8
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-09-26 18:03:49 -0300

    remove id from chat table

--- db/migrate/20220919170651_create_chats.rb
+++ db/migrate/20220919170651_create_chats.rb
@@ -2,8 +2,8 @@ class CreateChats < ActiveRecord::Migration[6.1]
   def change
     create_table :chats do |t|
       t.references :bike, foreign_key: true
-      t.references :seller_id, null: false, foreign_key: { to_table: :users }
-      t.references :buyer_id, null: false, foreign_key: { to_table: :users }
+      t.references :seller, null: false, foreign_key: { to_table: :users }
+      t.references :buyer, null: false, foreign_key: { to_table: :users }
       t.references :product, foreign_key: true
 
       t.timestamps
