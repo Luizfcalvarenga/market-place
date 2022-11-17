33942f538db34aca23fdd4908003378d8f6c96f0
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-11-16 23:12:56 -0300

    bike show some style adjustments

--- app/javascript/react_pages/bikes/Bike.jsx
+++ app/javascript/react_pages/bikes/Bike.jsx
@@ -13,7 +13,7 @@ export function Bike(props) {
 
   return (
 
-    <div className="product-show" bike={bike} key={bike} >
+    <div className="bike-show" bike={bike} key={bike} >
       {bike && (
         <div className="row row-cols-1">
           <div className="other-infos  col-12 col-md-8">
@@ -21,13 +21,13 @@ export function Bike(props) {
               <div className="carousel-inner">
                 <i className="fas fa-heart card-favorite"></i>
                 <div className="carousel-item active">
-                  <img src="https://www.bikemagazine.com.br/wp-content/uploads/2020/12/valeo-ebike.jpg" className="d-block w-100 img-show" alt="" />
+                  <img src="https://www.bikemagazine.com.br/wp-content/uploads/2020/12/valeo-ebike.jpg" className="d-block w-100 img-show-bike" alt="" />
                 </div>
                 <div className="carousel-item">
-                  <img src="https://www.bikemagazine.com.br/wp-content/uploads/2020/12/valeo-ebike.jpg" className="d-block w-100 img-show" alt="" />
+                  <img src="https://www.bikemagazine.com.br/wp-content/uploads/2020/12/valeo-ebike.jpg" className="d-block w-100 img-show-bike" alt="" />
                 </div>
                 <div className="carousel-item">
-                  <img src="https://www.bikemagazine.com.br/wp-content/uploads/2020/12/valeo-ebike.jpg" className="d-block w-100 img-show" alt="" />
+                  <img src="https://www.bikemagazine.com.br/wp-content/uploads/2020/12/valeo-ebike.jpg" className="d-block w-100 img-show-bike" alt="" />
                 </div>
               </div>
               <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -45,21 +45,21 @@ export function Bike(props) {
 
               <div className="d-flex justify-content-between">
                 <div>
-                  <h4 className="me-2 mb-3 text-black"><strong>Tamanho do quadro:</strong> <span className="text-success me-3">{bike.frame_size}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Material do quadro:</strong> <span className="text-success me-3">{bike.frame_material}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Tamanho do aro:</strong> <span className="text-success me-3">{bike.rim_size}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Marchas dianteiras:</strong> <span className="text-success me-3">{bike.number_of_front_gears}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Marchas traseiras:</strong> <span className="text-success me-3">{bike.number_of_rear_gears}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Tipo de freio:</strong> <span className="text-success me-3">{bike.brake_type}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Tamanho do quadro:</strong> <span className="me-3 bike-info">{bike.frame_size}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Material do quadro:</strong> <span className="me-3 bike-info">{bike.frame_material}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Tamanho do aro:</strong> <span className="me-3 bike-info">{bike.rim_size}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Marchas dianteiras:</strong> <span className="me-3 bike-info">{bike.number_of_front_gears}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Marchas traseiras:</strong> <span className="me-3 bike-info">{bike.number_of_rear_gears}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Tipo de freio:</strong> <span className="me-3 bike-info">{bike.brake_type}</span></h4>
                 </div>
 
                 <div>
-                  <h4 className="me-2 mb-3 text-black"><strong>Tipo de suspensão:</strong> <span className="text-success me-3">{bike.suspension_type}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Suspensão dianteira:</strong> <span className="text-success me-3">{bike.front_suspension_travel}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Suspensão traseira:</strong> <span className="text-success me-3">{bike.rear_suspension_travel}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Tipo de canote:</strong> <span className="text-success me-3">{bike.seat_post_type}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Curso do canote:</strong> <span className="text-success me-3">{bike.seat_post_travel}</span></h4>
-                  <h4 className="me-2 mb-3 text-black"><strong>Peso:</strong> <span className="text-success me-3">{bike.weight}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Tipo de suspensão:</strong> <span className="me-3 bike-info">{bike.suspension_type}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Suspensão dianteira:</strong> <span className="me-3 bike-info">{bike.front_suspension_travel}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Suspensão traseira:</strong> <span className="me-3 bike-info">{bike.rear_suspension_travel}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Tipo de canote:</strong> <span className="me-3 bike-info">{bike.seat_post_type}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Curso do canote:</strong> <span className="me-3 bike-info">{bike.seat_post_travel}</span></h4>
+                  <h4 className="me-2 mb-3 text-success"><strong>Peso:</strong> <span className="me-3 bike-info">{bike.weight}</span></h4>
                 </div>
               </div>
             </div>
@@ -68,17 +68,17 @@ export function Bike(props) {
               <h3 className="text-success mb-4">Condições e descrição</h3>
 
               <div className="d-flex justify-content-between">
-                <h4 className="me-2 mb-3 text-black"><strong>Condições da bike:</strong> <span className="text-success me-3">{bike.bike_conditions}</span></h4>
-                <h4 className="me-2 mb-3 text-black"><strong>Tipo de documento:</strong> <span className="text-success me-3">{bike.documentation_type}</span></h4>
+                <h4 className="me-2 mb-3 text-success"><strong>Condições da bike:</strong> <span className="me-3 bike-info">{bike.bike_conditions}</span></h4>
+                <h4 className="me-2 mb-3 text-success"><strong>Tipo de documento:</strong> <span className="me-3 bike-info">{bike.documentation_type}</span></h4>
               </div>
-              <h4 className="me-2 mb-3 text-black"><strong>Acompanha acessórios:</strong> <span className="text-success me-3">{bike.accessories  ? "Sim" : "Não"}</span></h4>
-              <h4 className="me-2 mb-3 text-black"><strong>Condições operacionais:</strong> <span className="text-success me-3">{bike.operating_condition}</span></h4>
-              <h4 className="me-2 mb-3 text-black"><strong>Condições estruturais:</strong> <span className="text-success me-3">{bike.structural_visual_condition}</span></h4>
-              <h4 className="me-2 mb-3 text-black"><strong>Descrição:</strong> <span className="text-success me-3">{bike.description}</span></h4>
+              <h4 className="me-2 mb-3 text-success"><strong>Acompanha acessórios:</strong> <span className="me-3 bike-info">{bike.accessories  ? "Sim" : "Não"}</span></h4>
+              <h4 className="me-2 mb-3 text-success"><strong>Condições operacionais:</strong> <span className="me-3 bike-info">{bike.operating_condition}</span></h4>
+              <h4 className="me-2 mb-3 text-success"><strong>Condições estruturais:</strong> <span className="me-3 bike-info">{bike.structural_visual_condition}</span></h4>
+              <h4 className="me-2 mb-3 text-success"><strong>Descrição:</strong> <span className="me-3 bike-info">{bike.description}</span></h4>
             </div>
           </div>
 
-          <div className="col-12 col-md-4 card-product">
+          <div className="col-12 col-md-4 card-bike">
             <div className="d-flex justify-content-between">
               <h3 className="card-title mt-3"> {bike.category.name}</h3>
               <i className="far fa-heart mt-4"></i>
