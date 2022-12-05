919400dd669f2d0df125bc3b373919e3295f9cf4
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-12-05 12:05:17 -0300

    bike in section for show page

--- app/javascript/react_pages/bikes/Bike.jsx
+++ app/javascript/react_pages/bikes/Bike.jsx
@@ -1,4 +1,16 @@
 import React, { useEffect, useState } from "react";
+import HandlebarImage from "../../../assets/images/handlebar.png";
+import CrankImage from "../../../assets/images/crank.png";
+import BrakesImage from "../../../assets/images/brakes.png";
+import SaddleImage from "../../../assets/images/saddle.png";
+import SuspensionImage from "../../../assets/images/suspension.png";
+import MountainBikeImage from "../../../assets/images/mountain-bike.png";
+import FrameImage from "../../../assets/images/frame.png";
+import AccessorieImage from "../../../assets/images/accessories.png";
+import EBikeImage from "../../../assets/images/e-bike.png";
+
+
+
 
 export function Bike(props) {
   const [bike, setBike] = useState()
@@ -40,41 +52,173 @@ export function Bike(props) {
               </button>
             </div>
 
-            <div className="technical-details-bike mt-5">
-              <h3 className="text-success mb-4">Características Técnicas</h3>
-
-              <div className="d-flex justify-content-between">
-                <div>
-                  <h4 className="me-2 mb-3 text-success"><strong>Tamanho do quadro:</strong> <span className="me-3 bike-info">{bike.frame_size}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Material do quadro:</strong> <span className="me-3 bike-info">{bike.frame_material}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Tamanho do aro:</strong> <span className="me-3 bike-info">{bike.rim_size}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Marchas dianteiras:</strong> <span className="me-3 bike-info">{bike.number_of_front_gears}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Marchas traseiras:</strong> <span className="me-3 bike-info">{bike.number_of_rear_gears}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Tipo de freio:</strong> <span className="me-3 bike-info">{bike.brake_type}</span></h4>
-                </div>
-
-                <div>
-                  <h4 className="me-2 mb-3 text-success"><strong>Tipo de suspensão:</strong> <span className="me-3 bike-info">{bike.suspension_type}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Suspensão dianteira:</strong> <span className="me-3 bike-info">{bike.front_suspension_travel}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Suspensão traseira:</strong> <span className="me-3 bike-info">{bike.rear_suspension_travel}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Tipo de canote:</strong> <span className="me-3 bike-info">{bike.seat_post_type}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Curso do canote:</strong> <span className="me-3 bike-info">{bike.seat_post_travel}</span></h4>
-                  <h4 className="me-2 mb-3 text-success"><strong>Peso:</strong> <span className="me-3 bike-info">{bike.weight}</span></h4>
+            <h3 className="text-success mb-4 mt-3">Características Técnicas</h3>
+            <ul class="main__menu">
+              <li class="list-item">
+                <a href="#" class="kabobs item--js">
+                  <span>Quadro</span>
+                  <img src={FrameImage} alt="" className="bike-part-card"/>
+                </a>
+                <ul class="drop-menu menu-2">
+                  <li class="drop-item"><p className="me-2 mb-3 text-success ms-2"><strong>Marca:</strong> <span className="me-3 bike-info">{bike.frame_brand}</span></p></li>
+                  <li class="drop-item"><p className="me-2 mb-3 text-success ms-2"><strong>Tamanho:</strong> <span className="me-3 bike-info">{bike.frame_size}</span></p></li>
+                  <li class="drop-item"><p className="me-2 mb-3 text-success ms-2"><strong>Material:</strong> <span className="me-3 bike-info">{bike.frame_material}</span></p></li>
+                </ul>
+              </li>
+            </ul>
+            <div className="row row-cols-1">
+              <div className="col-10 col-md-4">
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <img src={FrameImage} alt="" className="bike-part-card"/>
+                      <h4 className="mt-3 text-success">Quadro</h4>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4 className="mt-3 text-success">Quadro</h4>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Marca:</strong> <span className="me-3 bike-info">{bike.frame_brand}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Tamanho:</strong> <span className="me-3 bike-info">{bike.frame_size}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Material:</strong> <span className="me-3 bike-info">{bike.frame_material}</span></p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="technical-details-bike mt-5">
-              <h3 className="text-success mb-4">Condições e descrição</h3>
-
-              <div className="d-flex justify-content-between">
-                <h4 className="me-2 mb-3 text-success"><strong>Condições da bike:</strong> <span className="me-3 bike-info">{bike.bike_condition}</span></h4>
-                <h4 className="me-2 mb-3 text-success"><strong>Tipo de documento:</strong> <span className="me-3 bike-info">{bike.documentation_type}</span></h4>
+              <div className="col-10 col-md-4">
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <img src={CrankImage} alt="" className="bike-part-card"/>
+                      <h4 className="mt-3 text-success">Transmissão</h4>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4 className="mt-3 text-success">Transmissão</h4>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Nº Marchas(D):</strong> <span className="me-3 bike-info">{bike.number_of_front_gears}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Modelo:</strong> <span className="me-3 bike-info">{bike.front_derailleur_model}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Nº Marchas(T):</strong> <span className="me-3 bike-info">{bike.number_of_rear_gears}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Modelo:</strong> <span className="me-3 bike-info">{bike.rear_derailleur_model}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Pedivela:</strong> <span className="me-3 bike-info">{bike.crankset}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Corrente:</strong> <span className="me-3 bike-info">{bike.chain}</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div><div className="col-10 col-md-4">
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <img src={SuspensionImage} alt="" className="bike-part-card"/>
+                      <h4 className="mt-3 text-success">Suspensão</h4>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4 className="mt-3 text-success">Suspensão</h4>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Dianteira:</strong> <span className="me-3 bike-info">{bike.front_suspension_travel}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Modelo:</strong> <span className="me-3 bike-info">{bike.front_suspension_model}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Traseira:</strong> <span className="me-3 bike-info">{bike.rear_suspension_travel}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Modelo:</strong> <span className="me-3 bike-info">{bike.rear_suspension_model}</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div><div className="col-10 col-md-4 mt-3">
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <img src={BrakesImage} alt="" className="bike-part-card"/>
+                      <h4 className="mt-3 text-success">Freios</h4>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4 className="mt-3 text-success">Freios</h4>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Tipo:</strong> <span className="me-3 bike-info">{bike.brake_type}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Disco:</strong> <span className="me-3 bike-info">{bike.brake_disc_size}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Modelo:</strong> <span className="me-3 bike-info">{bike.brake_model}</span></p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <h4 className="me-2 mb-3 text-success"><strong>Acompanha acessórios:</strong> <span className="me-3 bike-info">{bike.accessories}</span></h4>
-              <h4 className="me-2 mb-3 text-success"><strong>Condições operacionais:</strong> <span className="me-3 bike-info">{bike.operating_condition}</span></h4>
-              <h4 className="me-2 mb-3 text-success"><strong>Condições estruturais:</strong> <span className="me-3 bike-info">{bike.structural_visual_condition}</span></h4>
-              <h4 className="me-2 mb-3 text-success"><strong>Descrição:</strong> <span className="me-3 bike-info">{bike.description}</span></h4>
+              <div className="col-10 col-md-4 mt-3">
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <img src={HandlebarImage} alt="" className="bike-part-card"/>
+                      <h4 className="mt-3 text-success">Cockpit</h4>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4 className="mt-3 text-success">Cockpit</h4>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Mesa:</strong> <span className="me-3 bike-info">{bike.stem}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Guidão:</strong> <span className="me-3 bike-info">{bike.handlebar}</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-10 col-md-4 mt-3">
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <img src={MountainBikeImage} alt="" className="bike-part-card"/>
+                      <h4 className="mt-3 text-success">Rodas</h4>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4 className="mt-3 text-success">Rodas</h4>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Tamanho:</strong> <span className="me-3 bike-info">{bike.rim_size}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Aro(D):</strong> <span className="me-3 bike-info">{bike.front_rim_model}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Aro(T):</strong> <span className="me-3 bike-info">{bike.rear_rim_model}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Pneu(D):</strong> <span className="me-3 bike-info">{bike.front_tyre}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Pneu(T):</strong> <span className="me-3 bike-info">{bike.rear_tyre}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Cubo(D):</strong> <span className="me-3 bike-info">{bike.front_hub}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Cubo(T):</strong> <span className="me-3 bike-info">{bike.rear_hub}</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-10 col-md-4 mt-3">
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <img src={SaddleImage} alt="" className="bike-part-card"/>
+                      <h4 className="mt-3 text-success">Canote</h4>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4 className="mt-3 text-success">Canote</h4>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Tipo:</strong> <span className="me-3 bike-info">{bike.seat_post_type}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Tamanho:</strong> <span className="me-3 bike-info">{bike.seat_post_travel}</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-10 col-md-4 mt-3">
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <img src={AccessorieImage} alt="" className="bike-part-card"/>
+                      <h4 className="mt-3 text-success">Acessórios</h4>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4 className="mt-3 text-success">Acessórios</h4>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Qual:</strong> <span className="me-3 bike-info">{bike.accessories}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Descrição:</strong> <span className="me-3 bike-info">{bike.accessories_description}</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {bike.bike_type === "e-bike" && (
+                <div className="col-10 col-md-4 mt-3">
+                <div class="flip-card">
+                  <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                      <img src={EBikeImage} alt="" className="bike-part-card"/>
+                      <h4 className="mt-3 text-success">Elétrica</h4>
+                    </div>
+                    <div class="flip-card-back">
+                      <h4 className="mt-3 text-success">Elétrica</h4>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Bateria:</strong> <span className="me-3 bike-info">{bike.battery}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Ciclos:</strong> <span className="me-3 bike-info">{bike.battery_cycles}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Km:</strong> <span className="me-3 bike-info">{bike.mileage}</span></p>
+                      <p className="me-2 mb-3 text-success ms-2"><strong>Motor:</strong> <span className="me-3 bike-info">{bike.motor}</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              )}
             </div>
           </div>
 
