a920e4cab9830d07a9ea1f5afbd6c843ded28c3c
Luiz Fernando Alvarenga <luizfca7@gmail.com>
2022-12-20 17:12:52 -0300

    search list in navbar details in front

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -4,28 +4,22 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "form", "list", "searchInput" ]
 
-  connect() {
-    // console.log(this.element);
-    // console.log(this.formTarget);
-    // console.log(this.listTarget);
-    // console.log(this.searchInputTarget);
-  }
+  connect() {}
 
   update() {
     const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
-
-
+    console.log(this.searchInputTarget.value)
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
       .then((data) => {
-
-
-          console.log(data);
-          this.listTarget.outerHTML = data;
-
+        console.log(data);
+        this.listTarget.outerHTML = data;
       })
 
 
+    if (this.searchInputTarget.value === null) {
+      this.listTarget.classList.add("d-none")
+    }
 
   }
 
