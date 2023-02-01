48bab4501d4c31885ed29fbc3f9f5f137611c4d5
Luiz Alvarenga <luizfca7@gmail.com>
2023-02-01 19:51:37 -0300

    fuckin stimulus dont get simple form as targetto submit

--- app/javascript/controllers/verify_controller.js
+++ app/javascript/controllers/verify_controller.js
@@ -1,14 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
+import Rails from "@rails/ujs"
 
 // Connects to data-controller="verify"
 export default class extends Controller {
   static targets = [ "form", "verified" ]
 
   connect() {
-    console.log("_opa")
+    console.log(this.verifiedTarget)
+
+    console.log(this.formTarget)
   }
   submit(e) {
     e.preventDefault();
+    Rails.fire(this.formTarget, 'submit')
+
     this.formTarget.submit();
   }
 }
