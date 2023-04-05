6347b87bc1ec4c40c1286d397b9ae20f73ce4aae
Luiz Alvarenga <luizfca7@gmail.com>
2023-04-04 22:58:08 -0300

    login error with method in sessions controller as test

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,11 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :disable_turbo, only: [:new, :create]
 
   # GET /resource/sign_in
   # def new
+  #   # binding.pry
   #   super
   # end
 
@@ -14,7 +16,6 @@ class Users::SessionsController < Devise::SessionsController
     #   order = Order.find_by(tracker_code: cookies[:tracker_code])
     #   order.update(user: resource) if order.user.blank? && !resource.orders.where(status: :pending).exists?
     # end
-
   end
 
   # DELETE /resource/sign_out
@@ -25,10 +26,14 @@ class Users::SessionsController < Devise::SessionsController
     super
   end
 
+  def disable_turbo
+    request.variant = :without_turbo
+  end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
 end
