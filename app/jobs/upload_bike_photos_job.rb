2264026e74be1cf695c30699ca78924cf3f630f0
Luiz Alvarenga <luizfca7@gmail.com>
2023-04-07 11:02:04 -0300

    remove comments and empty lines from job to upload photos

--- app/jobs/upload_bike_photos_job.rb
+++ app/jobs/upload_bike_photos_job.rb
@@ -2,24 +2,12 @@ class UploadBikePhotosJob < ApplicationJob
   queue_as :default
 
   def perform(bike, image_data_uri)
-
-    # bike.photos.attach(io: File.open(File.join(file_path_to_save_to)), filename: photo_name, content_type: photo_content_type)
     photo_decode = Base64.decode64(image_data_uri)
-
     image_blob = ActiveStorage::Blob.create_after_upload!(
       io: StringIO.new(photo_decode),
       filename: "image.jpg",
       content_type: "image/jpeg"
     )
-
-    # image_data = Base64.decode64(image_data_uri['data:image/png;base64,'.length .. -1])
-    # image_file = Tempfile.new('image_file')
-    # image_file.binmode
-    # image_file.write image_data
-    # image_file.rewind
     bike.photos.attach(image_blob)
-
-
-
   end
 end
--- app/jobs/upload_product_photos_job.rb
+++ app/jobs/upload_product_photos_job.rb
@@ -3,19 +3,11 @@ class UploadProductPhotosJob < ApplicationJob
 
   def perform(product, image_data_uri)
     photo_decode = Base64.decode64(image_data_uri)
-
     image_blob = ActiveStorage::Blob.create_after_upload!(
       io: StringIO.new(photo_decode),
       filename: "image.jpg",
       content_type: "image/jpeg"
     )
-
-    # image_data = Base64.decode64(image_data_uri['data:image/png;base64,'.length .. -1])
-    # image_file = Tempfile.new('image_file')
-    # image_file.binmode
-    # image_file.write image_data
-    # image_file.rewind
     product.photos.attach(image_blob)
-
   end
 end
