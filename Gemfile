5dcafeb131c482c3f461eab59dbe69c3844056f8
Luiz Alvarenga <luizfca7@gmail.com>
2023-05-22 16:54:09 -0300

    sso implementation

--- Gemfile
+++ Gemfile
@@ -48,6 +48,11 @@ gem 'uglifier'
 gem 'iugu', git: "https://github.com/iugu/iugu-ruby"
 gem 'active_model_serializers'
 
+
+gem 'omniauth'
+gem 'omniauth-oauth2'
+
+
 gem "turbo-rails"
 gem 'pg_search'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
 
 	include DeviseTokenAuth::Concerns::User
-
+  devise :omniauthable
+  
   scope :all_except, ->(user) {where.not(id: user)}
   after_create_commit { broadcast_append_to "users" }
   after_update_commit { broadcast_update }
