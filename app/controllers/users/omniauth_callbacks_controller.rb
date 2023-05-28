374c1042c152efeb4b66842f5eb7c2fdf0fc54f4
Luiz Alvarenga <luizfca7@gmail.com>
2023-05-28 18:48:22 -0300

    mismatch uri for googe sign in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,4 +3,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user
   end
+
+  # protected
+
+  # def after_omniauth_failure_path_for(_scope)
+  #   new_user_session_path
+  # end
+
+  # def after_sign_in_path_for(resource_or_scope)
+  #   stored_location_for(resource_or_scope) || root_path
+  # end
 end
