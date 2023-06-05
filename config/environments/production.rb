ed2e02de569424486f8a64cf5fbb1595761dc792
Luiz Alvarenga <luizfca7@gmail.com>
2023-06-05 18:32:21 -0300

    new review

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://market.nuflow.com.br/" }
+  config.action_mailer.default_url_options = { host: "market.nuflow.com.br" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
